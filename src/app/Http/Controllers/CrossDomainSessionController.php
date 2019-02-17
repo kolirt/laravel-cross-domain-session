0071029a389757796e2d554bc589bb1a489051ca
Asan Abdurakhmanov <asan11@outlook.com>
2019-02-17 08:58:26 +0200

    1.0.3

---
+++ src/app/Http/Controllers/CrossDomainSessionController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lopol
+ * Date: 17.02.2019
+ * Time: 8:56
+ */
+
+namespace Kolirt\CrossDomainSession\app\Http\Controllers;
+
+
+class CrossDomainSessionController
+{
+
+}
